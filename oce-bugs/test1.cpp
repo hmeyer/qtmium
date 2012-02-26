f6a9e713553c15c49c32fb108b0a8a0e80b043c2
Henning Meyer <hmeyer@gmx.eu>
2012-02-26 10:22:13 +0100

    stripped

--- oce-bugs/test1.cpp
+++ oce-bugs/test1.cpp
@@ -1,21 +1,10 @@
 #include <BRepBuilderAPI_MakeEdge2d.hxx>
-#include <BRepAlgo.hxx>
-#include <BRepAlgo_BooleanOperation.hxx>
-#include <BRepAlgoAPI_BooleanOperation.hxx>
 #include <BRepAlgoAPI_Cut.hxx>
-
 #include <Geom2d_Circle.hxx>
-#include <TopoDS.hxx>
-#include <TopoDS_Shape.hxx>
-#include <TopoDS_Edge.hxx>
-#include <gp.hxx>
 #include <gp_Circ2d.hxx>
 
-
 int main(int argc, char **argv){
-    TopoDS_Shape c1;
-    TopoDS_Shape c2;
-    
+    TopoDS_Shape c1,c2;
     c1 = BRepBuilderAPI_MakeEdge2d( Geom2d_Circle( gp::OX2d(), 1 ).Circ2d() ).Shape();
     c2 = BRepBuilderAPI_MakeEdge2d( Geom2d_Circle( gp::OX2d(), 2 ).Circ2d() ).Shape();
     BRepAlgoAPI_Cut cut(c1,c2);
